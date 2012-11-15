c7dfef04ff410177c9044477122a3a16ec80d5fc
ryanfiorini <ryanwfiorini@gmail.com>
2012-11-15 10:40:56 -0500

    repo init

---
+++ AppMobiPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using appMobiWebViewWP8;
+
+namespace AppMobiWP8HelloWorldTemplate
+{
+    public partial class AppMobiPage : PhoneApplicationPage
+    {
+        public appMobiWebView amWebView;
+
+        public AppMobiPage()
+        {
+            InitializeComponent();
+
+            // phone:WebBrowser will serve up the files
+            //amWebView = new appMobiWebView(webView, phoneApplicationPage, "html/index.html");
+
+            // appMobiServer will serve up the files
+            amWebView = new appMobiWebView(webView, phoneApplicationPage, "http://localhost:5888/index.html");
+        }
+    }
+}
---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AppMobiWP8ContactsTemplate.Resources;
+
+namespace AppMobiWP8ContactsTemplate
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
